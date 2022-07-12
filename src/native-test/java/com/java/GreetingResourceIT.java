a4b7dad4a97ac29d26dad45dbb212473e76cbcbe
Marco Seabra <marcogutto@brq.com>
2022-07-12 17:08:38 -0300

    Setup inicial ...

---
+++ src/native-test/java/com/java/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.java;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
